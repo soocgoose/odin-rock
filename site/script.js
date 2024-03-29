c247164012fc4a339b8a13d4f5b78eabe871d64e
Shane O. O'Connor <skyfoosooc@gmail.com>
2024-03-29 15:35:51 -0400

    Play game without the need to open console
    Results are now displayed within the 'game-results' and 'round-results'
    sections. After 5 rounds the game resets.

--- site/script.js
+++ site/script.js
@@ -9,23 +9,9 @@ function getComputerChoice() {
 	}
 }
 
-function showUserButtonChoice(elem) {
-	console.log(elem.target.id);
-}
-
 let buttons = document.querySelectorAll('#user-input > button');
 for (const btn of buttons) {
-	btn.addEventListener('click', showUserButtonChoice);
-}
-
-function getUserChoice() {
-	let validChoice = false;
-	do {
-		var choice = prompt("Rock Paper Scissors", getComputerChoice()).toLowerCase();
-		validChoice = (choice == 'rock' || choice == 'paper' || choice == 'scissors');
-
-	} while (!validChoice)
-	return choice;
+	btn.addEventListener('click', playGame);
 }
 
 function playRound(player1, player2) {
@@ -37,38 +23,42 @@ function playRound(player1, player2) {
 	} else if(player1 === 'rock' && player2 === 'scissors'
 		|| player1 === 'paper' && player2 === 'rock'
 		|| player1 === 'scissors' && player2 === 'paper') {
-		return {winner: 1, message: `${playerOneName} wins! ${player1} beats ${player2}`};
+		return {winner: 1, message: `${playerOneName} won! ${player1} beats ${player2}`};
 	} else {
-		return {winner: 2, message: `${playerTwoName} wins! ${player2} beats ${player1}`};
+		return {winner: 2, message: `${playerTwoName} won! ${player2} beats ${player1}`};
 	}
 }
 
-function playGame() {
-	let roundsRemaining = 5;
+let roundsRemaining = 5;
 
-	let playerOneWins = 0;
-	let playerTwoWins = 0;
+let playerOneWins = 0;
+let playerTwoWins = 0;
 
-	while (roundsRemaining > 0) {
-		let playerSelection = getUserChoice();
-		let computerSelection = getComputerChoice();
+function playGame(userButtonElement) {
+	document.querySelector('#game-results > p').textContent = `There are ${--roundsRemaining} more rounds in this game`;
 
-		let results = playRound(playerSelection, computerSelection);
+	let playerSelection = userButtonElement.target.textContent;
+	let computerSelection = getComputerChoice();
 
-		console.log(results.message);
-		roundsRemaining -= 1;
+	let results = playRound(playerSelection, computerSelection);
 
-		if (results.winner === 1) {
-			playerOneWins += 1;
-		} else if (results.winner === 2) {
-			playerTwoWins += 1;
-		}
+	document.querySelector('#round-results > p').textContent = results.message;
+
+	if (results.winner === 1) {
+		playerOneWins += 1;
+	} else if (results.winner === 2) {
+		playerTwoWins += 1;
 	}
 
-	if (playerOneWins === playerTwoWins) {
-		console.log("You both tied!");
-	} else {
-		console.log(`${(playerOneWins > playerTwoWins) ? playerOneName : playerTwoName} won!`);
+	if (roundsRemaining === 0) {
+		document.querySelector('#game-results > p').textContent
+			= (playerOneWins === playerTwoWins)
+			? "You both tied!"
+			: `${(playerOneWins > playerTwoWins) ? playerOneName : playerTwoName} won!`;
+
+		roundsRemaining = 5;
+		playerOneWins = 0;
+		playerTwoWins = 0;
 	}
 }
 
